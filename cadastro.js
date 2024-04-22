56d6d25ec1575a0da3ecc030bc4d45bfcdbeb7b1
igorbraga890 <160538899+igorbraga890@users.noreply.github.com>
2024-04-22 08:01:28 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,11 @@
+document.getElementById('teste').style.display = "block"
+
+function botaozinho(){
+    window.alert("No dia xx/xx/xxxx vá para a prefeitura e use os dados para se candidatar")
+}
+function botaozinho2(){
+    window.alert("No dia xx/xx/xxxx vá para a prefeitura e use os dados para se tornar um mesario")
+}
+function botaozinho3(){
+    window.alert("No dia xx/xx/xxxx vá para a prefeitura e use os dados para votar em seus candidatos")
+}
